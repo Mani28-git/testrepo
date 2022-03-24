78d88362594d5138ca137e119f796acb481b5ef7
Mani28-git <85493838+Mani28-git@users.noreply.github.com>
2022-03-24 17:33:12 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -4,6 +4,12 @@ resource "azurerm_resource_group" "rg-new" {
   tags =   var.tags
 }
 
+resource "azurerm_resource_group" "policy-new" {
+  name     = "policy-rg"
+  location = "West Europe"
+  tags =   var.tags
+}
+
 variable "tags" {
   type = map(string)
   default = {
