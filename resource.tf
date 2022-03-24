737d4c61947908e426900ad40be146191816b406
Mani28-git <85493838+Mani28-git@users.noreply.github.com>
2022-03-24 12:26:06 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "ec2-machine" {
+resource "aws_instance" "ec2-instance" {
   ami           = "ami-0d2aa5df6e106903e"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "HelloWorld-dev"
